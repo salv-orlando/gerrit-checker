72a616fed59496f4e51eb4956b905686e8e763ad
Salvatore Orlando <salv.orlando@gmail.com>
2014-05-28 07:34:46 -0700

    Add a daily routine shell
    This way I also know the stuff I need to do before considering
    this tool done.

---
+++ daily_routine.sh
@@ -0,0 +1,26 @@
+user_name=$1
+password=$2
+# check for new changes - ie: submitted since last check - in neutron (including specs)
+echo New Neutron patches
+gerrit-check --project openstack/neutron-specs openstack/neutron openstack/python-neutronclient --user $user_name --password $password --only-new --peek
+# check for specs from my team
+echo Team specs
+gerrit-check --project openstack/nova-specs openstack/neutron-specs --user $user_name --password $password --owners garyk arosen --peek
+# check for patches from my team, and add me as a reviewer
+echo Team patches
+gerrit-check --project openstack/tempest openstack/nova openstack/neutron openstack/python-neutronclient --user $user_name --password $password --owners garyk arosen --add-reviewer self --peek
+# check for neutron patches for vmware plugins, and add me as a reviewer
+echo VMware Neutron patches
+gerrit-check --project openstack/neutron --user $user_name --password $password --file ^neutron/plugins/vmware.* --exclude-owners $user_name --add-reviewer self --peek
+# check for neutron patches touching apis, and add me as a reviewer
+echo Neutron API patches
+gerrit-check --project openstack/neutron --user $user_name --password $password --file ^neutron/api/.* --exclude-owners $user_name --add-reviewer self --peek
+# check for neutron patches touching db package, and add me a reviewer
+echo Neutron DB patches
+gerrit-check --project openstack/neutron --user $user_name --password $password --file ^neutron/db/.* --exclude-owners $user_name --add-reviewer self --peek
+# check for neutron patches still without a reviewer (TODO)
+# check for tempest network patches still without a reviewer (TODO)
+# check for nova/neutron interface patches still without a reviewe (TODO)
+# check for patches where I'm a reviewer but haven't actually reviewed yet
+# check for patches which I'm reviewing and author has replied, and are now
+# waiting for another round of review (TODO)
